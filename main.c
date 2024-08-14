cd29ab08104c40459f84bd69188f769021bbde48
root <9548holbertonstudents.com>
2024-08-14 14:47:00 +0200

    command_lines_whith_arguments

---
+++ main.c
@@ -0,0 +1,34 @@
+#include "shell.h"
+
+/**
+ * main - entry point for our simple shell program, checks for interactive
+ *  or non-interactive
+ *
+ * @argc: count of command line arguments
+ * @argv: command line arguments array
+ *
+ * Return: 0 if successful exit
+*/
+
+int main(int argc, char **argv)
+{
+	char *input_line;
+	char *newline = "\n";
+
+	while (1)
+	{
+		prompt_and_read_input(&input_line);
+		if (!input_line)
+		{
+			if (isatty(STDIN_FILENO))
+			{
+				write(STDOUT_FILENO, newline, strlen(newline));
+			}
+			break;
+		}
+		process_command(input_line, argv, environ);
+		free(input_line);
+	}
+
+	return (0);
+}
