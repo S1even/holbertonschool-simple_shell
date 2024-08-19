d1d28b7d55b0c1e7de488be0dc9d6fb0fb88b9d0
S1even <stevendevin95520@hotmail.com>
2024-08-19 10:05:06 +0000

    Update new function

--- shell.h
+++ shell.h
@@ -7,19 +7,19 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
 
-int main(void);
+
 void print_prompt(void);
 ssize_t read_command(char **buffer, size_t *size);
-void execute_command(char **argv);
 char **parse_command(char *command);
-char *check_command_path(const char *dir, const char *command);
-char *find_command_in_path(char *command);
+void execute_command(char **parsed_command, char *program_name, char **env);
 void exit_shell(void);
 void env_variables(char **env);
 int check_path_executable(const char *path);
 char *create_command_path(char *command);
 void execute_in_child(char *cmd_path, char **parsed_command, char **env);
-void execute_command(char **parsed_command, char *program_name, char **env);
+char *find_command_in_path(char *command);
 
 #endif
