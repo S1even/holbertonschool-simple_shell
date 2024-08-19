fc185f8a636ca305a1c7171898740007e75e0273
root <dylan2003gd@gmail.com>
2024-08-19 09:09:44 +0000

    Update for can use find_command

--- execute_command.c
+++ execute_command.c
@@ -7,18 +7,24 @@
 
 void execute_command(char **argv)
 {
+	char *cmd_path = NULL;
 	struct stat st;
 
 	if (stat(argv[0], &st) == 0 && (st.st_mode & S_IXUSR))
-	{
-		if (execve(argv[0], argv, NULL) == -1)
-		{
-			perror("./shell");
-		}
-	}
+		cmd_path = argv[0];
 	else
 	{
-		perror("./shell");
+		cmd_path = find_command_in_path(argv[0]);
+		if (!cmd_path)
+		{
+			perror("./shell");
+			free(argv);
+			return;
+		}
 	}
+	if (execve(cmd_path, argv, NULL) == -1)
+		perror("./shell");
+	if (cmd_path != argv[0])
+		free(cmd_path);
 	free(argv);
 }
