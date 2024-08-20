1000b046c832e5965b66b592be02c97412cc3e83
S1even <Stevendevin95520@hotmail.com>
2024-08-20 11:55:27 +0200

    Update new funtion

--- shell.h
+++ shell.h
@@ -1,25 +1,25 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <stdio.h>
+#include <stddef.h>
 #include <unistd.h>
 #include <string.h>
-#include <stdlib.h>
+#include <stdio.h>
 #include <sys/wait.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include <sys/stat.h>
-#include <sys/types.h>
-#include <errno.h>
+
+#define MAX_CHARS 1024
+extern char environ;
 
 
+char token_path(char path, char delim, char filename);
+char **token_line(char *command_line, const char *delim);
+char command_path(char cmd);
+void free_argument(char **argum);
 void print_prompt(void);
-ssize_t read_command(char **buffer, size_t *size);
-char **parse_command(char *command);
-void execute_command(char **parsed_command, char *program_name, char **env);
-void exit_shell(void);
-void env_variables(char **env);
-int check_path_executable(const char *path);
-char *create_command_path(char *command);
-void execute_in_child(char *cmd_path, char **parsed_command, char **env);
-char *find_command_in_path(char *command);
+void execute_command(int *status, pid_t pid, char **argum, char *commandLine,
+		int *exit_status, char *argv);
 
 #endif
